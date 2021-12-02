4d1d410fb84aab35c107695c35d51474a0f005aa
oct0punk <55735176+CouetteAllan@users.noreply.github.com>
2021-12-02 14:28:11 +0100

    include SFML in the project

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
+
+#include "Tool.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float wHeight = 720;
+	float wWidth = 1280;
+	sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Jeu de ouf");
+	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(144);
+
+
+	double tStart = getTimeStamp();
+	double tEnterFrame = getTimeStamp();
+	double tExitFrame = getTimeStamp();
+
+
+	while (window.isOpen()) {
+		sf::Event event;
+		double dt = tExitFrame - tEnterFrame;
+		tEnterFrame = getTimeStamp(); //calculer le temps entre chaque frame pour les vitesses
+		while (window.pollEvent(event)) {
+			switch (event.type)
+			{
+
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+
+			default:
+				break;
+			}
+		}
+
+
+		window.clear();
+
+		window.display();
+		tExitFrame = getTimeStamp();
+	}
+
+	return 0;
 }
