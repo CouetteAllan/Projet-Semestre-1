34db128c458dc7487ad1044054beedfdb79060d7
Allan <couetteallan@gmail.com>
2021-12-14 15:26:06 +0100

    update + draw
    w

--- ConsoleApplication1/ConsoleApplication1/Entity.hpp
+++ ConsoleApplication1/ConsoleApplication1/Entity.hpp
@@ -13,6 +13,23 @@ public:
 	//---------------- Coordinates -------------------------------
 	int				cx = 0;
 	int				cy = 0;
+	float			rx = 0.0f;
+	float			ry = 0.0f;
+
+	//---------------  Movements   -------------------------------
 	float			dx = 0.0;
 	float			dy = 0.0f;
+
+	Entity(Shape* shape, int x, int y) {
+		spr = shape;
+		cx = x;
+		cy = y;
+	}
+
+	void update(double dt);
+	void draw(RenderWindow& win);
+
+protected:
+	void syncSprite();
+
 };
